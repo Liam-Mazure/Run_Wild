6801c3a8da4063952249a2d1f9cef54da6fe6d22
Mazureli <liammazure2001@gmail.com>
2024-07-02 23:16:43 -0400

    Initial Nav & Home

---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+
+
+function Navbar(){
+    return(
+        <nav>
+            <h1 id = "nav_title">RunWild</h1>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    Our Story
+                </li>
+                <li>
+                    Games
+                </li>
+                <li>
+                    Films
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+        </nav>
+    )
+}
